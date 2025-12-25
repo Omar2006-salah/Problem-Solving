b6040188f1580f664444264b650d5c9ead23e641
Omar2006-salah <osmatar2006@gmail.com>
2025-12-25 11:03:06 -0800

    Create Orac And LCM.cpp

---
+++ Orac And LCM.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        set<int> st;
+        for (int i = 1 ; i <= nums.size() ; i++) {
+            st.insert(i);
+        }
+        map<int , int> mp;
+        int r = -1;
+        for (auto &i : nums) {
+            mp[i]++;
+            if (mp[i] > 1) {
+                r = i;
+            }
+            if (st.count(i)) {
+                st.erase(i);
+            }
+        }
+        vector<int> ans(2);
+        ans[0] = r;
+        ans[1] = *st.begin();
+
+        return ans;
+    }
+};
+int main() {
+    Solution s;
+}
